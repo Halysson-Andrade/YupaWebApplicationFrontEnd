b2aea0239e9a290cc87f1c2f01fac715b9dbdd19
Yupa Dev <dev_yupa@itajubaazure.onmicrosoft.com>
2025-02-16 12:00:36 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'http://localhost:3009/standard/prod/api/v1',
+  apiURL: 'https://yupawebapplicationapi-almani.onrender.com/standard/prod/api/v1',
   apiDomain: 'http://localhost:3009',
   authRoute: '/standard/prod/api/v1/login',
   clientName: 'almani',
